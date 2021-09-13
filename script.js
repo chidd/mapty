fbdc8eff651cc73ed8c7dc834e1a909de23aa231
chinedu <chi.adams54@gmail.com>
2021-09-13 19:12:36 +0100

    Added the map marker on click event and set the L.popup method options to style popup content

--- script.js
+++ script.js
@@ -14,11 +14,10 @@ const inputElevation = document.querySelector('.form__input--elevation');
 // test if geolocation feature is available in the case of old browers
 if(navigator.geolocation)
     navigator.geolocation.getCurrentPosition(function(position){
-        console.log(position);
+
         const {latitude} = position.coords;//using obj destructuring to access latitude prop
         const {longitude} = position.coords;//using obj destructuring to access longitude prop
-        console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
-
+        
         const coords = [latitude,longitude];
 
         //  create and load a map using leaflet.js
@@ -28,9 +27,25 @@ if(navigator.geolocation)
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map);
 
-        L.marker(coords).addTo(map)
-            .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        
+        
+        // handle click event using leaflet.js special on method on the map object created using leaflet
+        map.on('click',function (mapEvent) {
+            // object destructuring used to get lat and lng values
+            const {lat,lng} = mapEvent.latlng;
+
+            L.marker([lat,lng]).addTo(map)
+            .bindPopup(L.popup({
+                maxWidth: 250,
+                minWidth: 100,
+                autoClose:false,
+                closeOnClick:false,
+                className:'running-popup'
+            }))
+            .setPopupContent('Workout Location')
             .openPopup();
+            console.log(lat,lng);
+        });
     },
     function(){
         alert("Couldn't get your current position");
