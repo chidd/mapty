ca0b8deaeb0e382cd51ed6d723792375d27a0da6
chinedu <chi.adams54@gmail.com>
2021-09-13 20:02:08 +0100

    rendering workout input form whenever the user clicks on the map

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+let map, mapEvent;
+
 // test if geolocation feature is available in the case of old browers
 if(navigator.geolocation)
     navigator.geolocation.getCurrentPosition(function(position){
@@ -20,8 +22,8 @@ if(navigator.geolocation)
         
         const coords = [latitude,longitude];
 
-        //  create and load a map using leaflet.js
-        var map = L.map('map').setView(coords, 13);
+        // load a map using leaflet.js
+        map = L.map('map').setView(coords, 13);
 
         L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -30,23 +32,40 @@ if(navigator.geolocation)
         
         
         // handle click event using leaflet.js special on method on the map object created using leaflet
-        map.on('click',function (mapEvent) {
-            // object destructuring used to get lat and lng values
-            const {lat,lng} = mapEvent.latlng;
+        map.on('click',function (mapE) {
+            mapEvent = mapE;            
+            form.classList.remove('hidden');
+            inputDistance.focus();
 
-            L.marker([lat,lng]).addTo(map)
-            .bindPopup(L.popup({
-                maxWidth: 250,
-                minWidth: 100,
-                autoClose:false,
-                closeOnClick:false,
-                className:'running-popup'
-            }))
-            .setPopupContent('Workout Location')
-            .openPopup();
-            console.log(lat,lng);
+
         });
     },
     function(){
         alert("Couldn't get your current position");
+    });
+
+    // Handle form submit
+    form.addEventListener('submit',function(e){
+        e.preventDefault();
+
+        // clear input field
+        inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value ='';
+        // Display the marker        
+        const {lat,lng} = mapEvent.latlng;// object destructuring used to get lat and lng values
+
+        L.marker([lat,lng]).addTo(map)
+        .bindPopup(L.popup({
+            maxWidth: 250,
+            minWidth: 100,
+            autoClose:false,
+            closeOnClick:false,
+            className:'running-popup'
+        }))
+        .setPopupContent('Workout Location')
+        .openPopup();
+    });
+
+    inputType.addEventListener('change',function(){
+        inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+        inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
     });
